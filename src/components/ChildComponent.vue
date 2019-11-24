d1d0c2eeb540d55b5be9af534fac24bd6ec38c6c
maitandat1507 <maitandat1507@gmail.com>
2019-11-24 12:53:35 +0700

    Init source

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h3>{{tada}}</h3>
+    <button @click="localPrintt">Test localPrintl</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['tada'],
+  data: function() {
+    return {
+      tada1: 4
+    }
+  },
+  methods: {
+    localPrintt: function() {
+      let vm = this
+      let tada = 3
+      console.log(this.tada)
+      console.log(this.tada1)
+      console.log(tada)
+    },
+  },
+  componenents: {
+
+  }
+}
+
+</script>
