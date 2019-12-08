464022d0b7615088c73f14af00587bdf45aaea7e
maitandat1507 <maitandat1507@gmail.com>
2019-12-08 15:03:45 +0700

    Add missing file for Search Async demo

---
+++ src/components/SearchBoxAsync.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="row">
+    <div class="offset-md-8 col-md-4 search-box mt-2">
+      <input type="text" class="form-control search-box__input"
+        placeholder="Search async"
+        v-model="searchContentAsync"
+        ref="searchContentAsync">
+      <i class="fa fa-search search-box__icon"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  data: function() {
+    return {
+      searchContentAsync: ''
+    }
+  },
+  watch: {
+    searchContentAsync: function(newValue) {
+      // --- WAY 5: async (using Actions in Vuex)
+      this.$store.dispatch('changeSearchContentAsync', newValue)
+    },
+  },
+  computed: {
+    ...mapGetters([
+      'getSearchContentAsync',
+    ]),
+  },
+  methods: {},
+}
+</script>
+
+<style lang="scss" scoped>
+.search-box {
+  &__input {
+    position: relative;
+    padding-right: 35px;
+  }
+
+  &__icon {
+    position: absolute;
+    right: 20px;
+    line-height: 35px;
+    top: 0;
+    height: 35px;
+    width: 35px;
+  }
+}
+</style>
